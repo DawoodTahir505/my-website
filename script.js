278809e36ddc3eb70fcb719f9eff19e1b374a966
Dawood <dawoodtahiir@gmail.com>
2026-08-03 17:43:53 +0500

    Remove AOS initialization on DOMContentLoaded
    Remove initialization of the AOS animation library.

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-// Initialize the AOS animation library
-document.addEventListener("DOMContentLoaded", function() {
-    AOS.init({
-        once: true, // whether animation should happen only once - while scrolling down
-        offset: 100, // offset (in px) from the original trigger point
-        duration: 800, // values from 0 to 3000, with step 50ms
-        easing: 'ease-in-out', // default easing for AOS animations
-    });
-});
+
